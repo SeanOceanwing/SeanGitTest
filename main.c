b2e6fc2fb2ccb45909ca61b7313b1af23695254b
Sean <sean.gao@oceanwing.com>
2017-08-28 14:08:59 +0800

    add 1012

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 void T1011() {
-  printf("T1011 is coding... \n");
+  printf("T1011 is coding ... \n");
+  printf("T1011 is testing ... \n");
+  printf("T1011 is release ... \n");
+}
+
+void T1012() {
+  printf("T1012 is coding ... \n");
 }
 
 int main() {
@@ -9,6 +15,7 @@ int main() {
   printf("%s \n\n", msg);
 
   T1011();
+  T1012();
 
   return 0;
 }
