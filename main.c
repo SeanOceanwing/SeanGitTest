b006dae507d51d5f183f8019329605e05730dabc
Sean <sean.gao@oceanwing.com>
2017-08-28 15:54:14 +0800

    1012 test pass

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ void T1012() {
   printf("T1012 is coding ... \n");
   printf("T1012 is testing ... \n");
   printf("T1012 is fix bug ... \n");
+  printf("T1012 is test pass ... \n");
 }
 
 int main() {
